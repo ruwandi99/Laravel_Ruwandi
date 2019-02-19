34082c006c897b732432d7f9d70c0b4fcafe5840
Petani Kode <contoh@petanikode.com>
2019-02-19 09:58:04 +0700

    eps 3

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
         <li class="nav-item">
           <a class="nav-link" href="{{route('admin.home')}}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span>
+            <span>Dashboard </span>
           </a>
         </li>
       </ul>
